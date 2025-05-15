c7df47de6a193f7b092639c57da4423fa19cc990
dhairyak668 <dhairyak668@gmail.com>
2025-05-15 16:49:42 -0500

    fixed db_host variable in app.py

--- app.py
+++ app.py
@@ -45,12 +45,12 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
-DB_CONNECTION_NAME = os.getenv("DB_CONNECTION_NAME")
+DB_HOST = os.getenv("DB_HOST")
 
 # Function to establish connection with Google Cloud SQL
 def get_db_connection():
     return pymysql.connect(
-        host="34.173.250.12",
+        host=DB_HOST,
         user=DB_USER,
         password=DB_PASSWORD,
         db=DB_NAME,
